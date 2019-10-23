3bfc3b9020e0a96916ee19ec9b20bf3370c43f9d
InfiniteImagition <geyangchengyc@163.com>
2019-10-23 17:28:04 +0800

    MainActivity

--- app/src/main/java/com/spur_u/android/MainActivity.java
+++ app/src/main/java/com/spur_u/android/MainActivity.java
@@ -2,12 +2,47 @@ package com.spur_u.android;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.main, menu);//创建菜单
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {//item选项监听
+        switch (item.getItemId()) {
+            case R.id.add_item:
+                Toast.makeText(this, "You clicked Add", Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.remove_item:
+                Toast.makeText(this, "You clicked Remove", Toast.LENGTH_SHORT).show();
+                break;
+            default:
+        }
+        return true;
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button button_1 = findViewById(R.id.button_1);
+        button_1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                finish();//销毁一个活动
+            }
+        });
+
     }
 }
