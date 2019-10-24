5424bebd3c1c3b36540ddd86bec84e25a67e9214
InfiniteImagition <geyangchengyc@163.com>
2019-10-24 15:41:11 +0800

    Entity: Task

---
+++ app/src/main/java/com/spur_u/android/Entity/Task.java
@@ -0,0 +1,22 @@
+package com.spur_u.android.Entity;
+
+public class Task {
+    private String name;
+    private int score;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getScore() {
+        return score;
+    }
+
+    public void setScore(int score) {
+        this.score = score;
+    }
+}
